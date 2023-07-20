07eb02330f8854c869bcb8a99e876196dd91b5f8
Lauren Miller <lmiller90@fordham.edu>
2023-07-20 19:20:07 -0400

    completed home-routes

--- Develop/controllers/home-routes.js
+++ Develop/controllers/home-routes.js
@@ -1,25 +1,42 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+// THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created
+// WHEN I click on an existing blog post
+// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+// WHEN I enter a comment and click on the submit button while signed in
+// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
+
+router.get('/', withAuth, async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    // GET all projects and JOIN with user data
+    
+    const postData = await Post.findAll({
+      //specifically post attributes:
+      attributes: ['id', 'title', 'content', 'date_created' ],
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['id','username'],
         },
+        {
+          model: Comment,
+          attributes: ['id', 'comment', 'user_id','post_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
       ],
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      posts, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -27,42 +44,35 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+//GET a single post from :id
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
+      //specifically post attributes:
+      attributes: ['id', 'title', 'content', 'date_created' ],
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['id','username'],
         },
+        {
+          model: Comment,
+          attributes: ['id', 'comment', 'user_id','post_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
       ],
-    });
+    })
 
-    const project = projectData.get({ plain: true });
+    //serialize
+    const post = postData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    //render
+    res.render('post-detail', {
+      ...post,
       logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
@@ -72,7 +82,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
