587c15cf00480a690b7363dfcffadf4bc73f0365
Lauren Miller <lmiller90@fordham.edu>
2023-07-20 17:27:52 -0400

    required authentication for user routes

--- Develop/controllers/api/user-routes.js
+++ Develop/controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../utils/auth.js');
 
 router.post('/', async (req, res) => {
   try {
@@ -48,7 +49,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
