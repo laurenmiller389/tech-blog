f6898c95dd20c179ec1b048f1715ee2a725964d0
Lauren Miller <lmiller90@fordham.edu>
2023-07-20 20:04:47 -0400

    completed controller index

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.subscribe('/dashboard', dashboardRoutes)
 
 module.exports = router;
